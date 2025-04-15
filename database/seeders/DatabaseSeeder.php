2765f149d45a5ab9d861d185e249dea38aabe044
irwan-g10 <irwangumilar111@gmail.com>
2025-04-15 10:41:16 +0700

    update completed project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\Review;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,26 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReview = random_int(5, 30);
+
+            Review::factory()->count($numReview)->good()->for($book)->create();
+        });
+        Book::factory(33)->create()->each(function ($book) {
+            $numReview = random_int(5, 30);
+
+            Review::factory()->count($numReview)->average()->for($book)->create();
+        });
+        Book::factory(33)->create()->each(function ($book) {
+            $numReview = random_int(5, 30);
+
+            Review::factory()->count($numReview)->bad()->for($book)->create();
+        });
     }
+
 }
